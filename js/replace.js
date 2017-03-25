e3347dd5022f04fbc2ca3e0dc66b5a61e0b9619e
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-03-25 15:06:18 -0400

    content script

--- js/replace.js
+++ js/replace.js
@@ -1,9 +1,5 @@
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
-        $('input[type=text]').change(function () {
-            if (this.value[this.value.length - 1] === ' ') {
-                console.log(this.value);
-            }
-        })
+        chrome.tabs.sendMessage(tabId, {'action': "hi"});
     }
 });
