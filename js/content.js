9ec0837f3ae6ecefa234e473d57e47a61c535fe6
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-03-25 17:51:18 -0400

    text replacement printing

--- js/content.js
+++ js/content.js
@@ -1,14 +1,15 @@
 //content.js
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if (message.action === "hi") {
+    if (message.action === "addListener") {
 
         var inputs = document.getElementsByTagName('input');
-
+        
         for (var i = 0; i < inputs.length; i++) {
             if (inputs[i].type === 'text' || inputs[i].type === 'textArea') {
                 inputs[i].addEventListener('change', function (event) {
                     var value = event.target.value;
+                    console.log(value);
                     chrome.runtime.sendMessage({
                         "text": value
                     });
