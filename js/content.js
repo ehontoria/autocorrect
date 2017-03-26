a6beede0281de25b4f6079c0106a297ce86dd7a1
Huntoona <Huntoon@live.com>
2017-03-26 09:42:19 -0400

    Add support for all textareas and made eventhandling more precise

--- js/content.js
+++ js/content.js
@@ -3,22 +3,26 @@
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === "addListener") {
 
-        var inputs = document.getElementsByTagName('input');
-        
+        var inputEls = Array.from(document.getElementsByTagName('input'));
+        var textareas = Array.from(document.getElementsByTagName('textarea'));
+
+        var inputs = inputEls.concat(textareas);
         for (var i = 0; i < inputs.length; i++) {
-            if (inputs[i].type === 'text' || inputs[i].type === 'textArea') {
-                inputs[i].addEventListener('change', function (event) {
+            if ((inputs[i].type === 'text' || inputs[i].type === 'textarea')) {
+                inputs[i].addEventListener('input', function (event) {
                     var value = event.target.value;
-                    console.log(value);
-                    chrome.runtime.sendMessage({
-                        "text": value
-                    }, {}, function(response) {
-                        console.log(response);
-                        
-                        if (response) {
-                            event.target.value = response;
-                        }
-                    });
+                    if (value.substring(value.length - 1) === ' ') {
+                        // console.log(value);
+                        chrome.runtime.sendMessage({
+                            "text": value
+                        }, {}, function(response) {
+                            console.log(response);
+
+                            if (response) {
+                                event.target.value = response;
+                            }
+                        });
+                    }
                 });
             }
         }
