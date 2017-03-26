8d5637c6f9f3429b502dfc558453431cb6843257
Olivia Wan <oliviawan97@ccs.neu.edu>
2017-03-25 22:38:07 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-fluff-you-autocorrect.
-HackBeanpot 2017.
+# fluff-you-autocorrect.
+
+HackBeanpot 2017. Made by [Emily Hontoria](https://github.com/hontoriae/a), [Alex Huntoon](https://github.com/huntoona), and [Olivia Wan](https://github.com/omwan).
+
+A chrome extension that replaces swear words in typed text with a random nice word.
+
+To install:
+
+* Check out the repo or download the zip and extract
+* Go to [chrome://extensions/](chrome://extensions/) and enable developer mode
+* Select "Load unpacked extension" and navigate to the download location
+* Open the extension in the Chrome menu and make sure "Turn on autocorrect" is checked
+* Try it out on any site with input text areas, like [here](https://github.com/hontoriae/autocorrect)
--- js/popup.js
+++ js/popup.js
@@ -1,31 +1,29 @@
 //popup.js
 
-$('#switch').on('change', function(event) {
+$('#switch').on('change', function (event) {
     chrome.storage.sync.set({
         'typeCorrectActive': this.checked
     }, function () {
         // Notify that we saved.
         console.log('Settings saved');
-        
+
     });
 
     console.log(this.ownerDocument.defaultView.chrome.extension.getBackgroundPage());
     this.ownerDocument.defaultView.chrome.extension.getBackgroundPage().isActive = this.checked;
-    
-//    console.log($(window));
-    
+
+
 });
 
 console.log(this);
 
-$(document).ready(function() {
+$(document).ready(function () {
     chrome.storage.sync.get('typeCorrectActive', function (items) {
         $('#switch').prop('checked', items.typeCorrectActive);
         console.log(items);
 
         console.log($('#switch').is(':checked'));
         console.log($);
-//        this.defaultView.chrome.extension.getBackgroundPage().isActive = $('#switch').is(':checked');
     });
 
 });
